c2090ed9754c0ceb56e8d0a01cfe66eb72fb7099
Daniel Servin <321298027@alumno.enp.unam.mx>
2022-06-08 17:08:01 -0500

    refactor(validacion_Acceso.php): Intentando hacer que la contra hasheada almacenada en la BD coincida con el hash de la contra que el usuario introduzca

--- dynamics/php/iniReg/validacion_Acceso.php
+++ dynamics/php/iniReg/validacion_Acceso.php
@@ -1,80 +1,58 @@
 <?php
     require "../config/config.php";    
     require "../config/common_queries.php";    
-    // require "./seguridad.php";
+    require "seguridad.php";
 
-    function generar_pimienta(){
-        $caracteres = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz');
-        $pimienta = '';
-        $partes_pimienta = array_rand($caracteres,4);
-        $pimienta = $caracteres[$partes_pimienta[0]].$caracteres[$partes_pimienta[1]].$caracteres[$partes_pimienta[2]].$caracteres[$partes_pimienta[3]].$caracteres[$partes_pimienta[4]];
-        return $pimienta;
-    };
     
-    function generar_sal(){
-        $sal = uniqid();
-        return $sal;
-    };
-
-    $salesita = generar_sal(); 
-    $pimientita = generar_sal(); 
-    
-
-    $con = connect();
+  
+   
     $tipo_U = (isset($_POST["tipo_User"]) && $_POST["tipo_User"] != "")? $_POST["tipo_User"] : false;
     //echo $tipo_U;
     if($con && $tipo_U != false)
     {   
-        $ident = (isset($_POST["identIdent"]) && $_POST["identIdent"] != "")? $_POST["identIdent"] : false;
-        $contra = (isset($_POST["contrasena"]) && $_POST["contrasena"] != "")? $_POST["contrasena"] : false;
 
+        $ident = (isset($_POST["identIdent"]) && $_POST["identIdent"] != "")? $_POST["identIdent"] : false;
+        $contrasena = (isset($_POST["contrasena"]) && $_POST["contrasena"] != "")? $_POST["contrasena"] : false;
+        // $hash_base='f206b7a2b1eb5d85ef066154f07392';
+        
+        
         if(mysqlExistRegistroAll($ident, $con, "usuario", "num_identificador") == NULL)
         {
             header("location: ./registroUsuarios.php?user=$tipo_U");
         }
-        else
+        else if(mysqlExistRegistroAll($ident, $con, "usuario", "num_identificador") != NULL && verificar_contra_pimienta($contrasena, $sal, $hash_base))
         {
-            $sql = "SELECT ID_USUARIO, ID_TIPO_USER, tipo_usuario, ID_GRADO, apodo, nombre FROM USUARIO NATURAL JOIN TIPO_USER WHERE num_identificador = '$ident'";
+            $sql = "SELECT ID_USUARIO, ID_TIPO_USER, tipo_usuario, ID_GRADO, apodo, nombre, password FROM USUARIO NATURAL JOIN TIPO_USER WHERE num_identificador = '$ident'";
             $query = mysqli_query($con, $sql);
             $datos = mysqli_fetch_array($query, MYSQLI_ASSOC);        
             //var_dump($datos);
-            
+        
             $ID = $datos["ID_USUARIO"];
             $id_TU = $datos["ID_TIPO_USER"];
             $tipo_user = $datos["tipo_usuario"];
             $id_grado = $datos["ID_GRADO"];
             $apodoo = $datos["apodo"];
             $name = $datos["nombre"];
-            $contra = $_POST["contrasena"];
-
-            $contra_hasheada = hash("sha256", $contra.$salesita.$pimientita);
-            echo $ID."<br>".$id_TU."<br>".$tipo_user."<br>".$id_grado."<br>".$apodoo."<br>".$name."<br>".$contra_hasheada;
- 
             
-
             
+            // echo $ID."<br>".$id_TU."<br>".$tipo_user."<br>".$id_grado."<br>".$apodoo."<br>".$name."<br>".$contra_hasheada;
             session_id("sesion-act");
             session_name("AULA_CW");
-        
-            
             session_start();
-        
-            $_SESSION["ID_U"] = $ID;
-            $_SESSION["nombre"] = $name;
-            $_SESSION["apodo"] = $apodoo;
-            $_SESSION["ID_TU"] = $id_TU;
-            $_SESSION["tipo_U"] = $tipo_user;
-            $_SESSION["grado"] = $id_grado;
-
-            // $contra_hasheada = hash("sha256", $contra.$sal.$pimienta);
-            // echo $contra_hasheada;
-            // echo '<br><br>'; 
-            
+                $_SESSION["ID_U"] = $ID;
+                $_SESSION["nombre"] = $name;
+                $_SESSION["apodo"] = $apodoo;
+                $_SESSION["ID_TU"] = $id_TU;
+                $_SESSION["tipo_U"] = $tipo_user;
+                $_SESSION["grado"] = $id_grado; 
+                header("location: ../pageGates/lobby.php");
             
 
-
-            header("location: ../pageGates/lobby.php");
+            
         }    
+        else{
+            echo "Inicie sesión con la contraseña correcta";
+        }
 
         /*
         if($tipo_U == 1)
