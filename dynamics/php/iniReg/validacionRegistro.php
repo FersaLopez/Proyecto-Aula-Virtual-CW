dd846b6497754dfebadda98f96e2a9a6b89cfe9b
Daniel Servin <321298027@alumno.enp.unam.mx>
2022-06-08 15:22:51 -0500

    feat(validacionRegistro.php):Añadiendo líneas de código que permitirán hashear las contraseñas registradas por los usuarios

--- dynamics/php/iniReg/validacionRegistro.php
+++ dynamics/php/iniReg/validacionRegistro.php
@@ -1,24 +1,43 @@
 <?php
     require "../config/config.php";    
-    require "../config/common_queries.php";    
-
+    require "../config/common_queries.php";
+    // require_once "seguridad.php";
+    
+    
     $con = connect();
-       
 
     $tipo_U = (isset($_POST["tipo_U"]) && $_POST["tipo_U"] != "")? $_POST["tipo_U"] : false;
-
     $nombre = (isset($_POST["nombre"]) && $_POST["nombre"] != "")? $_POST["nombre"] : false;
-
     $apell_pat = (isset($_POST["apellido_pat"]))? $_POST["apellido_pat"] : false;
     $apell_mat = (isset($_POST["apellido_mat"]))? $_POST["apellido_mat"] : false;
-    
     $contra = (isset($_POST["password"]) && $_POST["password"] != "")? $_POST["password"] : false;
-    
     $apodo = (isset($_POST["apodo"]))? $_POST["apodo"] : false;
     $correo = (isset($_POST["correo"]))? $_POST["correo"] : false;
-    //$correo = $_GET["correo"];
-
     $identf = (isset($_POST["num_ident"]) && $_POST["num_ident"] != "")? $_POST["num_ident"] : false;
+    
+    function generar_pimienta(){
+        $caracteres = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz');
+        $pimienta = '';
+        $partes_pimienta = array_rand($caracteres,2);
+        $pimienta = $caracteres[$partes_pimienta[0]].$caracteres[$partes_pimienta[1]];
+        return $pimienta;
+    };
+    
+    function generar_sal(){
+        $sal = uniqid();
+        return $sal;
+    };
+
+    function hasheando($contra){
+        global $contra;
+        $contra_hasheada = hash("sha256",$contra); 
+        return $contra_hasheada;
+    }
+    
+    $hasheo = hasheando($contra);
+    $pimientita = generar_pimienta(); 
+    $salesita = generar_sal(); 
+    $concat= $hasheo.$pimientita.$salesita;
 
     function esNumeroDeCuenta($identf)
     {
@@ -55,15 +74,19 @@
 
     function verRedRegistroCorrecto($query, $con, $correo)
     {
+        global $concat; 
+        global $hasheo; 
+        global $pimientita; 
+        global $salesita; 
         if($query == true)
         {
             
             $id_U = obtenerID("ID_USUARIO", $con, "USUARIO", "correo", $correo, true);
-            $ID = $id_U["ID_USUARIO"];
-            
-            
-            $sql = "SELECT ID_TIPO_USER, tipo_usuario, ID_GRADO, apodo, nombre FROM USUARIO NATURAL JOIN TIPO_USER WHERE ID_USUARIO = $ID";
+            $ID = $id_U["ID_USUARIO"];  
+            $sql = "SELECT ID_TIPO_USER, tipo_usuario, ID_GRADO, apodo, nombre, password, Sal FROM USUARIO NATURAL JOIN TIPO_USER WHERE ID_USUARIO = $ID";
             $query = mysqli_query($con, $sql);
+            
+
             //echo $sql."<br>";
             //var_dump($query);
 /*
@@ -78,11 +101,15 @@
             $tipo_user = $datos["tipo_usuario"];
             $id_grado = $datos["ID_GRADO"];
             $apodoo = $datos["apodo"];
-            $name = $datos["nombre"];
+            $name = $datos["nombre"]; 
+            $contra = $datos["password"];    
+                    
 
-            echo $id_TU."<br>".$tipo_user."<br>".$id_grado."<br>".$apodoo.$name;
+            // $concat = $datos["password"];
+            // $salesita = $datos["Sal"];
             
-        
+            echo $id_TU."<br>".$tipo_user."<br>".$id_grado."<br>".$apodoo."<br>".$name."<br>".$concat;
+          
             // session_unset();
             // session_destroy();
             
@@ -98,8 +125,9 @@
             $_SESSION["apodo"] = $apodoo;
             $_SESSION["ID_TU"] = $id_TU;
             $_SESSION["tipo_U"] = $tipo_user;
-            $_SESSION["grado"] = $id_grado;    
-
+            $_SESSION["grado"] = $id_grado;     
+            $_SESSION["password"] = $concat;        
+            
         }
         else
             echo "La peticion salio mal";
@@ -140,13 +168,17 @@
             }
 
             if($verif_Creacion == true)
-            {
+            {   
+                 
+                
                 $sql = "INSERT INTO USUARIO 
                         (ID_USUARIO, ID_ESTADO, ID_TIPO_USER, ID_GRADO, nombre, apodo, apellido_paterno, apellido_materno,
-                        num_identificador, correo, ruta_foto, estado_unico, telefono, password) VALUES 
-                        (0, 1, $tipo_U, NULL, '$nombre', '$apodo', '$apell_pat', '$apell_mat', '$identf', '$correo', NULL, NULL, NULL, '$contra')";
+                        num_identificador, correo, ruta_foto, estado_unico, telefono, password, Sal) VALUES 
+                        (0, 1, $tipo_U, NULL, '$nombre', '$apodo', '$apell_pat', '$apell_mat', '$identf', '$correo', NULL, NULL, NULL, '$concat', '$salesita')";
                 $query = mysqli_query($con, $sql);
                 verRedRegistroCorrecto($query, $con, $correo);
+                
+                
                 //var_dump($query);                
                 //echo "podes hacer registro";
             }
@@ -177,13 +209,15 @@
             }
 
             if($verif_Creacion == true)
-            {
+            {   
+                
                 $sql = "INSERT INTO USUARIO 
                         (ID_USUARIO, ID_ESTADO, ID_TIPO_USER, ID_GRADO, nombre, apodo, apellido_paterno, apellido_materno,
-                        num_identificador, correo, ruta_foto, estado_unico, telefono, password) VALUES 
-                        (0, 1, $tipo_U, NULL, '$nombre', '$apodo', NULL, NULL, '$identf', '$correo', NULL, NULL, NULL, '$contra')";
+                        num_identificador, correo, ruta_foto, estado_unico, telefono, password, Sal) VALUES 
+                        (0, 1, $tipo_U, NULL, '$nombre', '$apodo', NULL, NULL, '$identf', '$correo', NULL, NULL, NULL, '$concat','$salesita')";
                 $query = mysqli_query($con, $sql);
                 verRedRegistroCorrecto($query, $con, $correo);
+                
                 // var_dump($query);                
                 // echo "podes hacer registro";
             }
@@ -210,12 +244,14 @@
 
             if($verif_Creacion == true)
             {
+              
                 $sql = "INSERT INTO USUARIO 
                         (ID_USUARIO, ID_ESTADO, ID_TIPO_USER, ID_GRADO, nombre, apodo, apellido_paterno, apellido_materno,
-                        num_identificador, correo, ruta_foto, estado_unico, telefono, password) VALUES 
-                        (0, 1, $tipo_U, NULL, '$nombre', NULL, '$apell_pat', '$apell_mat', '$identf', '$correo', NULL, NULL, NULL, '$contra')";
+                        num_identificador, correo, ruta_foto, estado_unico, telefono, password, Sal) VALUES 
+                        (0, 1, $tipo_U, NULL, '$nombre', NULL, '$apell_pat', '$apell_mat', '$identf', '$correo', NULL, NULL, NULL, '$concat','$salesita')";
                 $query = mysqli_query($con, $sql);
                 verRedRegistroCorrecto($query, $con, $correo);
+                
                 // var_dump($query);                
                 // echo "podes hacer registro";
             }
